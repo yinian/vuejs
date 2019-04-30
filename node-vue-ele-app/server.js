24f13188210dc4362bffd444a9686e1f72d8eec4
viagra <1356971566@qq.com>
2019-04-30 12:51:34 +0800

    fixed

--- node-vue-ele-app/server.js
+++ node-vue-ele-app/server.js
@@ -1,30 +1,43 @@
-// 实例化
-const express = require("express")
-const mongoose = require("mongoose")
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 const app = express();
 
 // 引入users.js
-const users = require("./routes/api/users");
-
+const users = require('./routes/api/users');
 // DB config
-const db = require("./config/keys").mongoURI;
+const db = require('./config/keys').mongoURI;
 
-// Connect to mongoDB
-mongoose.connect(db, {
-        useNewUrlParser: true
-    })
-    .then(() => console.log("MongoDB Connected"))
-    .catch(err => console.log(err));
-// 没有页面，无法通过浏览器访问，可以模拟
+// 使用body-parser中间件
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+app.use(bodyParser.json());
+
+
 app.get("/", (req, res) => {
     res.send("Hello World!");
-})
+});
+
+// Connect to mongodb
+mongoose
+    .connect(
+        db, {
+            useNewUrlParser: true
+        }
+    )
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+// passport 初始化
+app.use(passport.initialize());
+require('./config/passport')(passport);
 
 // 使用routes
-app.use("/api/users", users);
+app.use('/api/users', users);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
-})
+});
