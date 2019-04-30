5f4d3c65d94f85acefaf187c561923e373b9fb30
viagra <1356971566@qq.com>
2019-04-30 12:52:08 +0800

    fixed

--- node-vue-ele-app/config/keys.js
+++ node-vue-ele-app/config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb://root:root@127.0.0.1:27017/test?retryWrites=true"
-}
+    mongoURI: 'mongodb://root:root@127.0.0.1:27017/test',
+    secretOrKey: 'secret'
+};
--- node-vue-ele-app/models/User.js
+++ node-vue-ele-app/models/User.js
@@ -1,7 +1,6 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
-
-//Create Schema
+// Create Schema
 const UserSchema = new Schema({
     name: {
         type: String,
@@ -22,9 +21,11 @@ const UserSchema = new Schema({
         type: String,
         required: true
     },
+
     date: {
         type: Date,
         default: Date.now
     }
 });
-module.exports = User = mongoose.model("users", UserSchema);
+
+module.exports = User = mongoose.model('users', UserSchema);
