5cc16a49b4a7b243133072221acc832b636b7b38
viagra <1356971566@qq.com>
2019-05-03 23:56:01 +0800

    资金网关

--- node-vue-ele-app/routes/api/profiles.js
+++ node-vue-ele-app/routes/api/profiles.js
@@ -49,15 +49,25 @@ router.get(
         session: false
     }),
     (req, res) => {
+
+        // const profileFields = {};
+        // if (req.body.type) profileFields.type = req.body.type;
+        // if (req.body.describe) profileFields.describe = req.body.describe;
+        // if (req.body.income) profileFields.income = req.body.income;
+        // if (req.body.expend) profileFields.expend = req.body.expend;
+        // if (req.body.cash) profileFields.cash = req.body.cash;
+        // if (req.body.remark) profileFields.remark = req.body.remark;
+        // new Profile(profileFields).save().then(profile => {
+        //     res.json(profile);
+        // });
         Profile.find()
             .then(profile => {
                 if (!profile) {
                     return res.status(404).json('没有任何内容');
                 }
-
                 res.json(profile);
-            })
-            .catch(err => res.status(404).json(err));
+            }).catch(err => res.status(404).json(er));
+
     }
 );
 
@@ -122,13 +132,12 @@ router.delete(
         session: false
     }),
     (req, res) => {
+
         Profile.findOneAndRemove({
-                _id: req.params.id
-            })
-            .then(profile => {
-                profile.save().then(profile => res.json(profile));
-            })
-            .catch(err => res.status(404).json('删除失败!'));
+            _id: req.params.id
+        }).then(profile => {
+            profile.save().then(profile => res.json(profile));
+        }).catch(err => res.status(404).json('删除失败'));
     }
 );
 
