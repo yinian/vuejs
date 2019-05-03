bb8d72b0b21e58e70f47031c9aead2937ff3d086
viagra <1356971566@qq.com>
2019-05-03 23:42:55 +0800

    请求拦截和响应

--- node-vue-ele-app/client/src/http.js
+++ node-vue-ele-app/client/src/http.js
@@ -20,8 +20,46 @@ function endLoading() { //使用Element loading-close 方法
 }
 
 // 请求拦截
+axios.interceptors.request.use(
+    config => {
+        //加载动画
+        startLoading();
+        if (localStorage.eleToken) {
+            config.headers.Authorization = localStorage.eleToken;
+        }
+        return config;
+    },
+    eror => {
+        return Promise.reject(error);
+    }
+)
+
 
 //响应拦截
+axios.interceptors.response.use(
+    response => {
+        //结束加载动画
+        endLoading();
+        return response;
+    },
+    error => {
+        //错误提醒
+        endLoading();
+        Message.error(error.response.data);
+        // 获取错误状态码
+        const {
+            status
+        } = error.response;
+        if (status == 401) {
+            Message.error('token值无效，请重新登录');
+            // 清除token
+            localStorage.removeItem('eleToken')
+                //页面跳转
+            router.push('/login')
+        }
+        return Promise.reject(error);
+    }
+)
 
 
 
