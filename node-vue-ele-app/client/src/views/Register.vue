47d3f667e4b41bcc1a66367f570aa969fd688008
viagra <1356971566@qq.com>
2019-05-03 23:51:51 +0800

    注册视图

--- node-vue-ele-app/client/src/views/Register.vue
+++ node-vue-ele-app/client/src/views/Register.vue
@@ -1,17 +1,111 @@
 <template>
   <div class="register">
-    <selection class="form_container">
+    <section class="form_container">
       <div class="manage_tip">
         <span class="title">米修在线后台管理系统</span>
+        <el-form
+          :model="registerUser"
+          :rules="rules"
+          class="registerForm"
+          ref="registerForm"
+          label-width="80px"
+        >
+          <el-form-item label="用户名" prop="name">
+            <el-input v-model="registerUser.name" placeholder="请输入用户名"></el-input>
+          </el-form-item>
+          <el-form-item label="邮箱" prop="email">
+            <el-input v-model="registerUser.email" placeholder="请输入邮箱"></el-input>
+          </el-form-item>
+          <el-form-item label="密码" prop="password">
+            <el-input v-model="registerUser.password" placeholder="请输入密码" type="password"></el-input>
+          </el-form-item>
+          <el-form-item label="确认密码" prop="password2">
+            <el-input v-model="registerUser.password2" placeholder="请确认密码" type="password"></el-input>
+          </el-form-item>
+          <el-form-item label="选择身份">
+            <el-select v-model="registerUser.identity" placeholder="请选择身份">
+              <el-option label="管理员" value="manager"></el-option>
+              <el-option label="员工" value="employee"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary" class="submit_btn" @click="submitForm('registerForm')">注 册</el-button>
+          </el-form-item>
+        </el-form>
       </div>
-    </selection>
+    </section>
   </div>
 </template>
 
 <script>
 export default {
   name: "register",
-  components: {}
+  components: {},
+  data() {
+    var validatePass2 = (rule, value, callback) => {
+      if (value !== this.registerUser.password) {
+        callback(new Error("两次输入密码不一致!"));
+      } else {
+        callback();
+      }
+    };
+    return {
+      registerUser: {
+        name: "",
+        email: "",
+        password: "",
+        password2: "",
+        identity: ""
+      },
+      rules: {
+        email: [
+          {
+            type: "email",
+            required: true,
+            message: "邮箱格式不正确",
+            trigger: "blur"
+          }
+        ],
+        password: [
+          { required: true, message: "密码不能为空", trigger: "blur" },
+          { min: 6, max: 30, message: "长度在 6 到 30 个字符", trigger: "blur" }
+        ],
+        password2: [
+          { required: true, message: "确认密码不能为空", trigger: "blur" },
+          {
+            min: 6,
+            max: 30,
+            message: "长度在 6 到 30 个字符",
+            trigger: "blur"
+          },
+          { validator: validatePass2, trigger: "blur" }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      console.log(formName);
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          this.$axios
+            .post("/api/users/register", this.registerUser)
+            .then(res => {
+              // 注册成功
+              this.$message({
+                message: "注册成功！",
+                type: "success"
+              });
+              this.$router.push("/login");
+            });
+          //   alert("submit");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    }
+  }
 };
 </script>
 <style scoped>
@@ -50,4 +144,3 @@ export default {
   width: 100%;
 }
 </style>
-
